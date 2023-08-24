416afddb9ad5705b44aa7bb29c3e588c80432f5c
mkravy <michaelkravy@gmail.com>
2023-08-24 15:03:18 +0700

    telegram->main

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-from ORM.GamesORM import Game
-from ORM.PlayersORM import Player
-from ORM.ResultsORM import Result
-from _log import log
+import telebot
 
-game = Game()
-result = Result()
-player = Player()
+# создаем объект бота
+bot = telebot.TeleBot('6341865034:AAHvb4vpFLO4b7jC0436bEnwR5IhbrT0EgQ')
 
-# План
-# 1. Занести результат партии в games
-# 2. Пересчитать лучший результат по тегу в results
-# 3. Пересчитать результат по игроку в players
+# обработчик команды "/start"
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    # создаем клавиатуру с кнопкой "Привет"
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    keyboard.row('Привет')
+    # отправляем пользователю сообщение и клавиатуру
+    bot.send_message(message.chat.id, 'Нажми на кнопку "Привет"', reply_markup=keyboard)
 
-# 1. Внесение результата партии в games
-game.insert_game()
+# обработчик нажатия на кнопку "Привет"
+@bot.message_handler(func=lambda message: message.text == 'Привет')
+def hello_handler(message):
+    # отправляем пользователю сообщение "Здравствуй"
+    bot.send_message(message.chat.id, 'Здравствуй')
 
-# 1.1 Ищем новых игроков для добавления в players
-players = player.select_players()
-_players = game.select_new_players()
-names = [] #Список для имен действующих игроков
-
-for p in players:
-    names.append(p.name)
-
-for p in _players:
-    name = p[0]
-    tag = name[:5]
-    if name not in names:
-        player.insert(name, tag)
-
-# 2. Пересчет результатов по тегу в results
-result.update_results(game, result)
-
-# 3. Пересчет результатов игрока в players
-player.update_players(player, result)
-
-# 4. Вывод результатов
-players = player.select_all()
-
-for player in players:
-    name = player.name
-    level = player.level
-    points = player.points
-    print(f"{name} | {level} | {points}")
+# запускаем бота
+bot.polling(none_stop=True)
